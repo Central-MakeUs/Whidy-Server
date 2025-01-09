fe73fb95a46c96b2f868851494c69f3e18795fba
hyungjin <hyungzin0309@naver.com>
2025-01-09 21:02:30 +0900

    feat : User domain

---
+++ src/main/java/com/spam/whidy/WhidyApplication.java
@@ -0,0 +1,13 @@
+package com.spam.whidy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WhidyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WhidyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/spam/whidy/WhidyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spam.whidy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WhidyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
