96cc98313aae702bc1d6d5f528d9bf4fa80d3c4a
hyungjin <hyungzin0309@naver.com>
2025-01-23 19:37:07 +0900

    feat : place search

--- src/main/java/com/spam/whidy/application/place/PlaceService.java
+++ src/main/java/com/spam/whidy/application/place/PlaceService.java
@@ -6,6 +6,7 @@ import com.spam.whidy.domain.place.Place;
 import com.spam.whidy.domain.place.repository.PlaceRepository;
 import com.spam.whidy.domain.place.PlaceType;
 import com.spam.whidy.dto.place.CafeDTO;
+import com.spam.whidy.dto.place.FreeSpaceDTO;
 import com.spam.whidy.dto.place.PlaceDTO;
 import com.spam.whidy.dto.place.PlaceSearchCondition;
 import lombok.RequiredArgsConstructor;
@@ -21,6 +22,12 @@ public class PlaceService {
 
     private final PlaceRepository placeRepository;
 
+    @Transactional
+    public Long save(Place place){
+        placeRepository.save(place);
+        return place.getId();
+    }
+
     public Place findById(Long id){
         return placeRepository.findById(id)
                 .orElseThrow(() -> new BadRequestException(ExceptionType.PLACE_NOT_FOUND));
@@ -35,5 +42,12 @@ public class PlaceService {
                 .orElseThrow(() -> new BadRequestException(ExceptionType.PLACE_NOT_FOUND));
         return CafeDTO.of(place);
     }
+
+    public FreeSpaceDTO findFreeSpace(PlaceType placeType, Long id) {
+        Place place = placeRepository.findByPlaceTypeAndId(placeType, id)
+                .orElseThrow(() -> new BadRequestException(ExceptionType.PLACE_NOT_FOUND));
+        return FreeSpaceDTO.of(place);
+    }
+
 }
 
