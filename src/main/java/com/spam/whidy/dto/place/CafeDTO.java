83c8e4ff55b8a0cc73a4ce1c8d1d8f0d7477c607
hyungjin <hyungzin0309@naver.com>
2025-02-28 15:30:00 +0900

    feat : place

--- src/main/java/com/spam/whidy/dto/place/CafeDTO.java
+++ src/main/java/com/spam/whidy/dto/place/CafeDTO.java
@@ -4,11 +4,18 @@ import com.spam.whidy.domain.place.BusinessHour;
 import com.spam.whidy.domain.place.Place;
 import com.spam.whidy.domain.place.PlaceType;
 import com.spam.whidy.domain.place.additionalInfo.cafe.CafeAdditionalInfo;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
+import java.util.List;
 import java.util.Set;
 
 @Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class CafeDTO {
 
     private Long id;
@@ -17,10 +24,12 @@ public class CafeDTO {
     private double latitude;
     private double longitude;
     private Integer beveragePrice;
+    private Integer reviewNum;
     private Float reviewScore;
     private PlaceType placeType;
     private CafeAdditionalInfo additionalInfo;
     private Set<BusinessHour> businessHours;
+    private List<String> images;
 
     public static CafeDTO of(Place place){
         CafeDTO cafeDTO = new CafeDTO();
@@ -30,10 +39,12 @@ public class CafeDTO {
         cafeDTO.latitude = place.getLatitude();
         cafeDTO.longitude = place.getLongitude();
         cafeDTO.beveragePrice = place.getBeveragePrice();
+        cafeDTO.reviewNum = place.getReviewNum();
         cafeDTO.reviewScore = place.getReviewScore();
         cafeDTO.placeType = place.getPlaceType();
         cafeDTO.additionalInfo = (CafeAdditionalInfo) place.getAdditionalInfo();
         cafeDTO.businessHours = place.getBusinessHours();
+        cafeDTO.images = place.getImages();
         return cafeDTO;
     }
 }
