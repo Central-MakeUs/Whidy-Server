cea2d6de56c4c6e9ba6d9ca751061e10ec0b5860
hyungjin <hyungzin0309@naver.com>
2025-03-04 17:47:57 +0900

    test : controller tests

--- src/test/java/com/spam/whidy/testConfig/ControllerTest.java
+++ src/test/java/com/spam/whidy/testConfig/ControllerTest.java
@@ -1,24 +1,24 @@
 package com.spam.whidy.testConfig;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.spam.whidy.application.auth.AuthService;
+import com.spam.whidy.application.auth.token.AuthTokenService;
 import com.spam.whidy.common.config.auth.AuthArgumentResolver;
+import com.spam.whidy.common.config.jwt.JWTFilter;
 import com.spam.whidy.common.config.jwt.TokenUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.security.core.token.TokenService;
 import org.springframework.test.web.servlet.MockMvc;
 
 public abstract class ControllerTest {
 
-    @Autowired
-    protected MockMvc mockMvc;
+    @Autowired protected MockMvc mockMvc;
+    @Autowired protected ObjectMapper objectMapper;
+    @Autowired protected AuthArgumentResolver authArgumentResolver;
 
-    @Autowired
-    protected AuthArgumentResolver authArgumentResolver;
-
-    @MockBean
-    protected TokenUtil tokenUtil;
-
-    @MockBean
-    protected TokenService tokenService;
+    @MockBean protected TokenUtil tokenUtil;
+    @MockBean protected JWTFilter jwtFilter;
+    @MockBean protected AuthService authService;
+    @MockBean protected AuthTokenService authTokenService;
 
 }
